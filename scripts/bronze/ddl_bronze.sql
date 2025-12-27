c063c70884b157558ede238775552ec1c5f963f1
deekshithk1729 <deekshithk1729@gmail.com>
2025-12-27 10:51:40 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -60,7 +60,7 @@ IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE bronze.erp_loac_a101;
 GO
 
-CREATE TABLE bronze.erp_loac_a101 (
+CREATE TABLE bronze.erp_loc_a101 (
 	cid   NVARCHAR(50),
 	cntry NVARCHAR(50),
 );
